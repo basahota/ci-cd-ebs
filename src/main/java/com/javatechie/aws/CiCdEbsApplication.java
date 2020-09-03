c04f538e39780a070a286a0a10fef5cc3a77c208
Basant kumar Hota <basahota@users.noreply.github.com>
2020-09-03 13:57:25 +0530

    added path param

--- src/main/java/com/javatechie/aws/CiCdEbsApplication.java
+++ src/main/java/com/javatechie/aws/CiCdEbsApplication.java
@@ -3,6 +3,7 @@ package com.javatechie.aws;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class CiCdEbsApplication {
 
-	@GetMapping
-	public String welcome(){
-		return "Welcome to Java techie";
-	}
+    @GetMapping("/{name}")
+    public String welcome(@PathVariable String name) {
+        return name + " Welcome to Java techie";
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(CiCdEbsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdEbsApplication.class, args);
+    }
 
 }
